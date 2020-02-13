4e1c2e454cc132bb45229fe1c655dacbd8a6e60d
Jongkeun <jongkeun.kr@gmail.com>
2020-02-13 18:36:30 +0900

    Updates

---
+++ precache-manifest.3fc4b01b45defb9fffcd509ce4e7d62f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bafd407acb24161192467bce1c5bad9",
+    "url": "/todo-list-typescript/index.html"
+  },
+  {
+    "revision": "658a3fea0e2c7ce6dd95",
+    "url": "/todo-list-typescript/static/css/main.4c0e63f7.chunk.css"
+  },
+  {
+    "revision": "fd0ec84931a11dbba106",
+    "url": "/todo-list-typescript/static/js/2.62f063a0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/todo-list-typescript/static/js/2.62f063a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658a3fea0e2c7ce6dd95",
+    "url": "/todo-list-typescript/static/js/main.e87e577c.chunk.js"
+  },
+  {
+    "revision": "4f51c8fcc08ce56741ce",
+    "url": "/todo-list-typescript/static/js/runtime-main.a51495f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Jongkeun/todo-list-typescript/precache-manifest.1b8d145c84eefd9ad69beab471da291d.js"
+  "/todo-list-typescript/precache-manifest.3fc4b01b45defb9fffcd509ce4e7d62f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jongkeun/todo-list-typescript/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-list-typescript/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
